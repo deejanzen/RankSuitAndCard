59fa7a396c648489d295b67030e79a291793b701
deejanzen <djanzen@email.arizona.edu>
2016-01-21 10:38:52 -0700

    added duplicate checking

---
+++ src/DuplicateCardException.java
@@ -0,0 +1,5 @@
+/**
+ * Created by djanzen on 1/21/16.
+ */
+public class DuplicateCardException extends RuntimeException {
+}
--- src/PokerHand.java
+++ src/PokerHand.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * Created by djanzen on 1/20/16.
@@ -9,12 +10,19 @@ public class PokerHand {
     private List<Card> hand;
 
     public PokerHand(Card a, Card b, Card c, Card d, Card e){
-        hand = new ArrayList<>();
-        hand.add(a);
-        hand.add(b);
-        hand.add(c);
-        hand.add(d);
-        hand.add(e);
+        hand = new ArrayList<Card>();
+        Stack<Card> temp = new Stack<Card>();
+        temp.push(a);
+        temp.push(b);
+        temp.push(c);
+        temp.push(d);
+        temp.push(e);
+
+        while ( !temp.empty() ){
+            if ( !hand.contains( temp.peek() ) )  hand.add( temp.pop() );
+            else throw new DuplicateCardException();
+        }
+
         Collections.sort(hand);
 
     }
